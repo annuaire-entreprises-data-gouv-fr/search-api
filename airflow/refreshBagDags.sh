48182a62c4cadac21bf83d2dc2ee8e67e98b4e75
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-03-11 17:49:17 +0100

    refactor(airflow): delete airflow from api code

--- airflow/refreshBagDags.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo docker exec -it "${PWD##*/}"_webserver_1 python -c "from airflow.models import DagBag; d = DagBag();"
-docker exec -it "${PWD##*/}"_webserver_1 python -c "from airflow.models import DagBag; d = DagBag();"
-
--- airflow/scripts/airflow-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# airflow resetdb
-airflow db reset
-airflow db init
-# airflow upgradedb
-airflow db upgrade
-airflow users create -r Admin -u "$AIRFLOW_ADMIN_MAIL" -e "$AIRFLOW_ADMIN_MAIL" -f "$AIRFLOW_ADMIN_FIRSTNAME" -l "$AIRFLOW_ADMIN_NAME" -p "$AIRFLOW_ADMIN_PASSWORD"
-airflow scheduler &
-airflow webserver

741b45890d113315c79a1c9e05a70784177c4372
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-04-22 19:15:41 +0200

    style: fix BLACK formatting error

--- aio/aio-proxy/aio_proxy/search/index.py
+++ aio/aio-proxy/aio_proxy/search/index.py
@@ -8,6 +8,7 @@ class Siren(Document):
     Class used to represent a company headquarters,
     one siren number and the corresponding sheadquarters siret number
     """
+
     class Index:
         name = f'siren-{os.getenv("CURRENT_COLOR")}'
         settings = {"number_of_shards": 1, "number_of_replicas": 0}

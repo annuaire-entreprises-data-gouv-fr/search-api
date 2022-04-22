89d540fb2fe5539b8ecd4852e0a4ca04aae05f5d
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-04-22 16:02:24 +0200

    style: fix BLACK error

--- aio/aio-proxy/aio_proxy/helper.py
+++ aio/aio-proxy/aio_proxy/helper.py
@@ -2,7 +2,6 @@ import json
 
 
 def serialize_error_text(text: str) -> str:
-    """Serialize a text string to a JSON formatted string
-    """
+    """Serialize a text string to a JSON formatted string"""
     message = {"message": text}
     return json.dumps(message)

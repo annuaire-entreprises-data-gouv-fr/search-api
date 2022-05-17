f7ca58df0fc3b6a9e03f1dc3f7d7daf0c3374655
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-05-17 10:19:40 +0200

    feat: add swagger config

--- aio/aio-proxy/aio_proxy/main.py
+++ aio/aio-proxy/aio_proxy/main.py
@@ -1,9 +1,19 @@
 import logging
+import pathlib
 
 import aiohttp
 from aio_proxy.routes import routes
 from aio_proxy.settings import config
+from aiohttp_swagger import *
 from aiohttp import web
+from aiohttp_apispec import (docs,
+                             request_schema,
+                             response_schema,
+                             setup_aiohttp_apispec)
+
+
+BASE_DIR = pathlib.Path(__file__).parent.parent
+swagger = BASE_DIR / "aio_proxy" / "doc" / "swagger.yml"
 
 
 def main():
@@ -14,8 +24,18 @@ def main():
     app["http_session"] = aiohttp.ClientSession()
     app.router.add_routes(routes)
 
+    # setup_swagger(app, swagger_url="/doc", ui_version=2)
+    setup_swagger(app,
+                  swagger_url="/doc",
+                  swagger_from_file=swagger,
+                  )
+    '''
+    setup_aiohttp_apispec(app=app, title="My Documentation", version="v1",
+                          swagger_path="/docs",)
+    '''
     app["config"] = config
     web.run_app(app, host=config["host"], port=config["port"])
+    app.on_startup.append(swagger)
 
 
 if __name__ == "__main__":

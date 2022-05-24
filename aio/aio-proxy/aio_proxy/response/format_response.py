9c155472df037d15b9c42985801f37346eb25308
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-05-24 17:19:28 +0200

    style: fix linting bugs

--- aio/aio-proxy/aio_proxy/response/format_response.py
+++ aio/aio-proxy/aio_proxy/response/format_response.py
@@ -1,4 +1,5 @@
 import json
+
 from aio_proxy.response.helpers import set_default
 
 
@@ -14,15 +15,9 @@ def format_response(results):
                 "tranche_effectif_salarie": set_default(
                     result, "tranche_effectif_salarie_siege"
                 ),
-                "date_debut_activite": set_default(
-                    result, "date_debut_activite_siege"
-                ),
-                "etat_adiministratif": set_default(
-                    result, "etat_administratif_siege"
-                ),
-                "activite_principale": set_default(
-                    result, "activite_principale_siege"
-                ),
+                "date_debut_activite": set_default(result, "date_debut_activite_siege"),
+                "etat_adiministratif": set_default(result, "etat_administratif_siege"),
+                "activite_principale": set_default(result, "activite_principale_siege"),
                 "numero_voie": set_default(result, "numero_voie"),
                 "type_voie": set_default(result, "type_voie"),
                 "libelle_voie": set_default(result, "libelle_voie"),
@@ -41,17 +36,13 @@ def format_response(results):
             "tranche_effectif_salarie": set_default(
                 result, "tranche_effectif_salarie_unite_legale"
             ),
-            "date_mise_a_jour": set_default(
-                result, "date_mise_a_jour_unite_legale"
-            ),
+            "date_mise_a_jour": set_default(result, "date_mise_a_jour_unite_legale"),
             "categorie_entreprise": set_default(result, "categorie_entreprise"),
             "etat_administratif": set_default(
                 result, "etat_administratif_unite_legale"
             ),
             "nom_raison_sociale": set_default(result, "nom_raison_sociale"),
-            "nature_juridique": set_default(
-                result, "nature_juridique_unite_legale"
-            ),
+            "nature_juridique": set_default(result, "nature_juridique_unite_legale"),
             "activite_principale": set_default(
                 result, "activite_principale_unite_legale"
             ),
@@ -59,14 +50,17 @@ def format_response(results):
                 result, "economie_sociale_solidaire_unite_legale"
             ),
             "nom_complet": set_default(result, "nom_complet"),
-            "nombre_etablissements": int(set_default(
-                result, "nombre_etablissements", default=1
-            )),
-            "nombre_etablissements_ouverts": int(set_default(
-                result, "nombre_etablissements_ouverts", default=0
-            )),
-            "is_entrepreneur_individuel": json.loads(set_default(
-                result, "is_entrepreneur_individuel", default="false").lower()),
+            "nombre_etablissements": int(
+                set_default(result, "nombre_etablissements", default=1)
+            ),
+            "nombre_etablissements_ouverts": int(
+                set_default(result, "nombre_etablissements_ouverts", default=0)
+            ),
+            "is_entrepreneur_individuel": json.loads(
+                set_default(
+                    result, "is_entrepreneur_individuel", default="false"
+                ).lower()
+            ),
         }
         formatted_results.append(result_formatted)
     return formatted_results

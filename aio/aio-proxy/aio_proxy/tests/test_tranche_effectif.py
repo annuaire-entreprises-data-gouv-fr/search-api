12cfa68f7aa8666ed2e4cd688f89dd9b08440ade
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-05-24 16:17:31 +0200

    style: fix BLACK bug

--- aio/aio-proxy/aio_proxy/tests/test_tranche_effectif.py
+++ aio/aio-proxy/aio_proxy/tests/test_tranche_effectif.py
@@ -11,12 +11,7 @@ from aio_proxy.parsers.tranche_effectif import (
 def test_validate_tranche_effectif_salarie(
     tranche_effectif_salarie: str, expected: str
 ):
-    assert (
-        validate_tranche_effectif_salarie(
-            tranche_effectif_salarie
-        )
-        == expected
-    )
+    assert validate_tranche_effectif_salarie(tranche_effectif_salarie) == expected
 
 
 @pytest.mark.parametrize("tranche_effectif_salarie", ["000000", "0"])
@@ -26,9 +21,7 @@ def test_validate_tranche_effectif_salarie_fail_1(
     with pytest.raises(
         ValueError, match="Tranche salariés doit contenir 2 " "caractères."
     ):
-        validate_tranche_effectif_salarie(
-            tranche_effectif_salarie
-        )
+        validate_tranche_effectif_salarie(tranche_effectif_salarie)
 
 
 @pytest.mark.parametrize("tranche_effectif_salarie", ["54", "AN"])
@@ -36,6 +29,4 @@ def test_validate_tranche_effectif_salarie_fail_2(
     tranche_effectif_salarie: str,
 ):
     with pytest.raises(ValueError, match="Tranche salariés non valide."):
-        validate_tranche_effectif_salarie(
-            tranche_effectif_salarie
-        )
+        validate_tranche_effectif_salarie(tranche_effectif_salarie)

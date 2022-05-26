9830b1331f42d0d586042d033a2a57e96cb9e610
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-05-26 22:10:43 +0200

    test: add 'section activite principale' unit tests

---
+++ aio/aio-proxy/aio_proxy/tests/test_section_activite_principale.py
@@ -0,0 +1,22 @@
+import pytest
+from aio_proxy.parsers.section_activite_principale import (
+    validate_section_activite_principale,
+)
+
+
+@pytest.mark.parametrize(
+    "section_activite_principale, expected",
+    [("A", "A"), ("l", "l"), (None, None)],
+)
+def test_validate_section_activite_principale(
+    section_activite_principale: str, expected: str
+):
+    assert validate_section_activite_principale(section_activite_principale) == expected
+
+
+@pytest.mark.parametrize("section_activite_principale", ["Z", "68.20B"])
+def test_validate_section_activite_principale_fail(
+    section_activite_principale: str,
+):
+    with pytest.raises(ValueError, match="Section d'activité principale non valide."):
+        validate_section_activite_principale(section_activite_principale)

836af7870041561f8de16364fcddd8d3a272dc58
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-06-03 10:35:38 +0200

    feat: add timeout to urlopen

--- aio/aio-proxy/aio_proxy/search/helpers.py
+++ aio/aio-proxy/aio_proxy/search/helpers.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.DEBUG)
 def get_current_color(color_url):
     """Get current Elasticsearch index color from json file stored in MinIO."""
     try:
-        with urlopen(color_url) as url:
+        with urlopen(color_url, timeout=5) as url:
             data = json.loads(url.read().decode())
             current_color = data["CURRENT_COLOR"]
             logging.info(

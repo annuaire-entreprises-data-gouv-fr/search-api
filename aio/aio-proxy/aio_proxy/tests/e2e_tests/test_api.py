2ba5816536228233b86fd5997afac30a69287739
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-06-28 10:12:20 +0200

    CI: remove json from tests

--- aio/aio-proxy/aio_proxy/tests/e2e_tests/test_api.py
+++ aio/aio-proxy/aio_proxy/tests/e2e_tests/test_api.py
@@ -8,12 +8,10 @@ base_url = "http://localhost:4500/"
 def test_fetch_company():
     path = "search?q=ganymede"
     response = requests.get(url=base_url+path)
-    response_json = json.loads(response.text)
     assert response.status_code == 200
 
 
 def test_error():
     path = "search?qs=ganymede"
     response = requests.get(url=base_url+path)
-    response_json = json.loads(response.text)
     assert response.status_code == 400

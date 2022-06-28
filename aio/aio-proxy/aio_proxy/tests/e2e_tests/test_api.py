8e53db1c06902005f1a974b5e632d2194efc176f
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-06-28 13:02:24 +0200

    CI: test total results

--- aio/aio-proxy/aio_proxy/tests/e2e_tests/test_api.py
+++ aio/aio-proxy/aio_proxy/tests/e2e_tests/test_api.py
@@ -1,5 +1,7 @@
 # importing the requests library
 import requests
+import json
+from jsonpath_ng import jsonpath, parse
 
 # api-endpoint
 base_url = "http://localhost:4500/"
@@ -8,10 +10,12 @@ base_url = "http://localhost:4500/"
 def test_fetch_company():
     path = "search?q=ganymede"
     response = requests.get(url=base_url+path)
+    response_json = json.loads(response.text)
     assert response.status_code == 200
+    assert jsonpath.jsonpath(response_json, '$.total_results') > 10
 
 
-def test_error():
+def test_error_query():
     path = "search?qs=ganymede"
     response = requests.get(url=base_url+path)
     assert response.status_code == 400

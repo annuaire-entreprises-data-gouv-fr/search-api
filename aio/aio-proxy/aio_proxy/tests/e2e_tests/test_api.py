674551059427d9d87f2c56c2ae48f28c93c65627
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-06-29 14:55:09 +0200

    CI: remove logging

--- aio/aio-proxy/aio_proxy/tests/e2e_tests/test_api.py
+++ aio/aio-proxy/aio_proxy/tests/e2e_tests/test_api.py
@@ -1,6 +1,5 @@
 # importing the requests library
 import requests
-import logging
 
 # api-endpoint
 base_url = "http://localhost:4500/"
@@ -12,7 +11,6 @@ def test_fetch_company():
     response_json = response.json()
     total_results = response_json["total_results"]
     siren = response_json["results"][0]["siren"]
-    logging.info(f"****** siren de la poste: {siren} ******")
     assert response.status_code == 200
     assert total_results > 10
     assert siren == "356000000"

f3f658238132a77d5662e5bca3dcea9ca23275ad
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-09-07 15:15:06 +0200

    docs: add dirigeant filters to swagger

--- aio/aio-proxy/aio_proxy/parsers/date_parser.py
+++ aio/aio-proxy/aio_proxy/parsers/date_parser.py
@@ -4,7 +4,7 @@ from datetime import date, datetime
 
 @value_exception_handler(
     error="Veuillez indiquer une date sous le format : aaaa-mm-jj. Exemple : "
-          "'1990-01-02'"
+    "'1990-01-02'"
 )
 def parse_date(request, param: str, default_value=None):
     """Extract date param from request.
@@ -29,7 +29,7 @@ def parse_date(request, param: str, default_value=None):
 )
 def validate_dates(min_date=None, max_date=None):
     if min_date and max_date:
-        min_date = datetime.strptime(min_date, '%Y-%m-%d')
-        max_date = datetime.strptime(max_date, '%Y-%m-%d')
+        min_date = datetime.strptime(min_date, "%Y-%m-%d")
+        max_date = datetime.strptime(max_date, "%Y-%m-%d")
         if max_date < min_date:
             raise ValueError

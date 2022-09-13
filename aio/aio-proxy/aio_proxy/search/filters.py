d2b4f0bec9674d9264d69098137252bce4a0cf61
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-09-13 14:45:16 +0200

    fix: replace param name with param value

--- aio/aio-proxy/aio_proxy/search/filters.py
+++ aio/aio-proxy/aio_proxy/search/filters.py
@@ -3,5 +3,5 @@ def filter_search(search, filters_to_ignore: list, **params):
     # params is the list of parameters (filters) provided in the request
     for param_name, param_value in params.items():
         if param_value is not None and param_name not in filters_to_ignore:
-            search = search.filter("term", **{param_name: param_name})
+            search = search.filter("term", **{param_name: param_value})
     return search

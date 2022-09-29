426f6554041510cffcb709efd62cc7e27cec40bd
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-09-29 09:06:16 +0200

    fix: return only bool when checking siren number

--- aio/aio-proxy/aio_proxy/search/helpers.py
+++ aio/aio-proxy/aio_proxy/search/helpers.py
@@ -33,12 +33,12 @@ def get_current_color(color_url):
 CURRENT_COLOR = get_current_color(os.getenv("COLOR_URL"))
 
 
-def is_siren(query_string: str):
+def is_siren(query_string: str) -> bool:
     """
     Check if string is siren (composed of 9 digits).
     """
     if query_string is None:
-        return None
+        return False
     clean_query_string = query_string.replace(" ", "").upper()
     siren_valides = r"\b\d{9}\b"
     if re.search(siren_valides, clean_query_string):

a97f058515d337d96e4202a8988ccc8eeb88a9bc
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-09-29 07:40:08 +0200

    tests: add is_siren unit tests

---
+++ aio/aio-proxy/aio_proxy/tests/unit_tests/test_is_siren.py
@@ -0,0 +1,18 @@
+import pytest
+from aio_proxy.search.helpers import (
+    is_siren,
+)
+
+
+@pytest.mark.parametrize(
+    "siren, expected",
+    [
+        ("123456789", True),
+        ("123 456 689", True),
+        (None, None),
+        ("mother of dragons", "False"),
+        ("12345678", "False"),
+    ],
+)
+def test_is_siren(is_siren: str, expected: boolean):
+    assert is_siren(siren) == expected

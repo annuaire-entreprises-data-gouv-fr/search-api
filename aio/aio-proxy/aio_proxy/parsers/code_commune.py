894baddada6bcb5568fef3bae72987f7b7b8da4d
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-10-24 11:24:05 +0200

    fix: use regex instead of array to validate code commune

--- aio/aio-proxy/aio_proxy/parsers/code_commune.py
+++ aio/aio-proxy/aio_proxy/parsers/code_commune.py
@@ -1,6 +1,5 @@
 from typing import Optional
-
-from aio_proxy.labels.helpers import codes_communes
+import re
 
 
 def validate_code_commune(code_commune_clean: str) -> Optional[str]:
@@ -20,6 +19,7 @@ def validate_code_commune(code_commune_clean: str) -> Optional[str]:
         return None
     if len(code_commune_clean) != 5:
         raise ValueError("Code commune doit contenir 5 caractères !")
-    if code_commune_clean not in codes_communes:
+    codes_valides = r"^([013-9]\d|2[AB1-9])\d{3}$"
+    if not re.search(codes_valides, code_commune_clean):
         raise ValueError("Code commune non valide.")
     return code_commune_clean

0cfec3f1ad7b37b3906f34c57b0b45b13b84c6ea
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-11-10 17:01:54 +0100

    tests: add est_association filter test

---
+++ aio/aio-proxy/aio_proxy/tests/unit_tests/test_association.py
@@ -0,0 +1,22 @@
+import pytest
+from aio_proxy.parsers.bool_fields import validate_bool_field
+from aio_proxy.parsers.rge import validate_id_rge
+
+
+@pytest.mark.parametrize(
+    "est_association, expected",
+    [("TRUE", True), ("FALSE", False), (None, None)],
+)
+def test_validate_est_association(est_association, expected):
+    assert validate_bool_field("est_association", est_association) == expected
+
+
+@pytest.mark.parametrize("est_association", ["NON", "OUI", "NO", "YES"])
+def test_validate_est_association_fail(
+    est_association: str,
+):
+    with pytest.raises(
+        ValueError,
+        match="est_association doit prendre la valeur 'true' or 'false' !",
+    ):
+        validate_bool_field("est_association", est_association)

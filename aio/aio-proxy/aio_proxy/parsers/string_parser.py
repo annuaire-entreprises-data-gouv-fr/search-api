77aa31b8ddb641288d643dc836df870326451cf0
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-11-14 11:33:37 +0100

    feat: add type_personne to search algo

--- aio/aio-proxy/aio_proxy/parsers/string_parser.py
+++ aio/aio-proxy/aio_proxy/parsers/string_parser.py
@@ -1,11 +1,10 @@
-def clean_parameter(request, param: str, default_value=None):
+def clean_parameter(request, param: str):
     """Extract and clean param from request.
     Remove white spaces and use upper case.
 
     Args:
         request: HTTP request
         param (str): parameter to extract from request
-        default_value:
 
     Returns:
         None if None.

69ff71b4ddf86f556bb9863e567b848382ba9697
HAEKADI <hajar.aitelkadi.o@gmail.com>
2022-12-29 10:55:31 +0100

    feat: add etablissements geo search

--- aio/aio-proxy/aio_proxy/search/search_functions.py
+++ aio/aio-proxy/aio_proxy/search/search_functions.py
@@ -181,11 +181,33 @@ def search_geo(index, offset: int, page_size: int, **params):
     s = index.search()
     # Use filters to reduce search results
     s = filter_search(s, filters_to_ignore=["lat", "lon", "radius"], **params)
+    geo_query = {
+        "nested": {
+            "path": "etablissements",
+            "query": {
+                "bool": {
+                    "filter": {
+                        "geo_distance": {
+                            "distance": f"{params['radius']}km",
+                            "etablissements.coordonnees": {
+                                "lat": params["lat"],
+                                "lon": params["lon"],
+                            },
+                        }
+                    }
+                }
+            },
+            "inner_hits": {},
+        }
+    }
+    s = s.query(Q(geo_query))
+    """
     s = s.filter(
         "geo_distance",
         distance=f'{params["radius"]}km',
         coordonnees={"lat": params["lat"], "lon": params["lon"]},
     )
+    """
     return sort_and_execute_search(
         search=s, offset=offset, page_size=page_size, is_search_fields=True
     )

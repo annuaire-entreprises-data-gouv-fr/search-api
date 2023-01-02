db9d5b55b2371b84850bb20bbccb28ed01f10d46
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-02 08:22:32 +0100

    tests: add Retry object to add attempts to get requests

--- aio/aio-proxy/aio_proxy/tests/e2e_tests/test_api.py
+++ aio/aio-proxy/aio_proxy/tests/e2e_tests/test_api.py
@@ -4,10 +4,16 @@ import requests
 # api-endpoint
 base_url = "http://localhost:4500/"
 
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
+
 
 def test_fetch_company():
     path = "search?q=la poste"
-    response = requests.get(url=base_url + path)
+    response = session.get(url=base_url + path)
     response_json = response.json()
     total_results = response_json["total_results"]
     siren = response_json["results"][0]["siren"]
@@ -23,7 +29,7 @@ def test_personne_filter():
         "&date_naissance_personne_max"
         "=2000-01-01"
     )
-    response = requests.get(url=base_url + path)
+    response = session.get(url=base_url + path)
     response_json = response.json()
     total_results = response_json["total_results"]
     siren = response_json["results"][0]["siren"]
@@ -34,35 +40,35 @@ def test_personne_filter():
 
 def test_format_date_naissance():
     path = "search?date_naissance_personne_min=13/09/2001"
-    response = requests.get(url=base_url + path)
+    response = session.get(url=base_url + path)
     assert response.status_code == 400
 
 
 def test_error_query():
     path = "search?qs=ganymede"
-    response = requests.get(url=base_url + path)
+    response = session.get(url=base_url + path)
     assert response.status_code == 400
 
 
 def test_accept_three_characters():
     path = "search?q=abc"
-    response = requests.get(url=base_url + path)
+    response = session.get(url=base_url + path)
     assert response.status_code == 200
 
 
 def test_query_too_short():
     path = "search?q=ab"
-    response = requests.get(url=base_url + path)
+    response = session.get(url=base_url + path)
     assert response.status_code == 400
 
 
 def test_short_query_with_filter():
     path = "search?q=ab&code_postal=75015"
-    response = requests.get(url=base_url + path)
+    response = session.get(url=base_url + path)
     assert response.status_code == 200
 
 
 def test_terms_empty_only():
     path = "search?q="
-    response = requests.get(url=base_url + path)
+    response = session.get(url=base_url + path)
     assert response.status_code == 400

146ff92e5de9c00bdfb744b6ee2ac6fa4267746c
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-02 08:03:39 +0100

    feat: remove bool and id filters for etablissements functions

--- aio/aio-proxy/aio_proxy/search/filters/boolean.py
+++ aio/aio-proxy/aio_proxy/search/filters/boolean.py
@@ -2,11 +2,11 @@ from aio_proxy.search.helpers.elastic_fields import get_elasticsearch_field_name
 from elasticsearch_dsl import Q
 
 
-def filter_search_by_bool_variables_unite_legale(
-    search, filters_to_process: list, **params
+def filter_search_by_bool_fields_unite_legale(
+    search, filters_to_include: list, **params
 ):
     for param_name, param_value in params.items():
-        if param_value is not None and param_name in filters_to_process:
+        if param_value is not None and param_name in filters_to_include:
             if param_value:
                 search = search.filter(
                     "exists", field=get_elasticsearch_field_name(param_name)
@@ -18,61 +18,4 @@ def filter_search_by_bool_variables_unite_legale(
                         Q("exists", field=get_elasticsearch_field_name(param_name))
                     ],
                 )
-    return search
-
-
-def filter_search_by_bool_variables_etablissements(
-    search, filters_to_process: list, **params
-):
-    """
-    The parameters concerned by this function are bool variables that have a
-    `TRUE` value when they exist, and are not indexed (in elasticsearch) when the
-    value is missing, which is equivalent to having a `FALSE` value.
-    e.g "convention_collective_renseignee",
-        "est_finess",
-        "est_uai",
-        "est_entrepreneur_spectacle",
-        "est_rge",
-    In order to keep the elasticsearch index light, when the value is present for
-    these variables, we index `TRUE`, and when the value is missing (meaning it's
-    False), we do not index the value `FALSE`, we simply leave it empty.
-    Instead, we use this function to filter these values, based on whether a
-    value was indexed.
-    Having a value equals >> TRUE and not having an indexed value equals >> FALSE.
-    We use the "exists" elasticsearch filter to implement it.
-    """
-    # params is the list of parameters (filters) provided in the request
-    for param_name, param_value in params.items():
-        if param_value is not None and param_name in filters_to_process:
-            field = get_elasticsearch_field_name(param_name)
-            if param_value:
-                exists_query = {
-                    "nested": {
-                        "path": "etablissements",
-                        "query": {
-                            "bool": {
-                                "must": [
-                                    {"exists": {"field": "etablissements." + field}}
-                                ]
-                            }
-                        },
-                        "inner_hits": {},
-                    }
-                }
-                search = search.query(Q(exists_query))
-            else:
-                not_exists_query = {
-                    "nested": {
-                        "path": "etablissements",
-                        "query": {
-                            "bool": {
-                                "must_not": [
-                                    {"exists": {"field": "etablissements." + field}}
-                                ]
-                            }
-                        },
-                        "inner_hits": {},
-                    }
-                }
-                search = search.query(Q(not_exists_query))
     return search

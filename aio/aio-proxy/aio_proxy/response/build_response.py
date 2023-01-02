c03f48d629621c342e0bb00d17c7590a7e996c7c
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-02 22:05:51 +0100

    feat: add elasticsearch query execution time to dev env response

--- aio/aio-proxy/aio_proxy/response/build_response.py
+++ aio/aio-proxy/aio_proxy/response/build_response.py
@@ -1,10 +1,17 @@
 import json
+import os
 from typing import Callable, Dict
 
 from aio_proxy.decorators.http_exception import http_exception_handler
 from aio_proxy.response.format_response import format_response
 from aio_proxy.search.index import ElasticsearchSireneIndex
 from aiohttp import web
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+env = os.getenv("ENV")
 
 
 @http_exception_handler
@@ -22,7 +29,7 @@ def api_response(
         total_pages)
     """
     parameters, page, per_page = extract_function(request)
-    total_results, results = search_function(
+    total_results, results, execution_time = search_function(
         ElasticsearchSireneIndex, page * per_page, per_page, **parameters
     )
     results_formatted = format_response(results)
@@ -38,4 +45,7 @@ def api_response(
         if remainder_results == 0
         else res["total_results"] // res["per_page"] + 1
     )
+
+    if env == "dev":
+        res["execution_time"] = execution_time
     return web.json_response(text=json.dumps(res, default=str))

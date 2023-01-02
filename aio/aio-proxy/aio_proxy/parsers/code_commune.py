196f98401c94d5534c02068b74d61c69bbf09cb3
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-02 09:01:31 +0100

    feat: parse code commune as list of strings instead of string

--- aio/aio-proxy/aio_proxy/parsers/code_commune.py
+++ aio/aio-proxy/aio_proxy/parsers/code_commune.py
@@ -1,25 +1,27 @@
 import re
-from typing import Optional
+from typing import List, Optional
 
 
-def validate_code_commune(code_commune_clean: str) -> Optional[str]:
-    """Check the validity of code_commune.
+def validate_code_commune(list_code_commune_clean: List[str]) -> Optional[List[str]]:
+    """Check the validity of list_code_commune.
 
     Args:
-        code_commune_clean(str, optional): code commune extracted and cleaned.
+        list_code_commune_clean(list(str), optional): list of codes commune extracted
+        and cleaned.
 
     Returns:
         None if code_commune_clean is None.
-        code_commune_clean if valid.
+        list_code_commune_clean if valid.
 
     Raises:
-        ValueError: if code_commune_clean not valid.
+        ValueError: if one of the values in list_code_commune_clean is not valid.
     """
-    if code_commune_clean is None:
+    if list_code_commune_clean is None:
         return None
-    if len(code_commune_clean) != 5:
-        raise ValueError("Code commune doit contenir 5 caractères !")
-    codes_valides = r"^([013-9]\d|2[AB1-9])\d{3}$"
-    if not re.search(codes_valides, code_commune_clean):
-        raise ValueError("Code commune non valide.")
-    return code_commune_clean
+    for code_commune in list_code_commune_clean:
+        if len(code_commune) != 5:
+            raise ValueError("Chaque code commune doit contenir 5 caractères !")
+        codes_valides = r"^([013-9]\d|2[AB1-9])\d{3}$"
+        if not re.search(codes_valides, code_commune):
+            raise ValueError("Au moins un des codes communes est non valide.")
+    return list_code_commune_clean

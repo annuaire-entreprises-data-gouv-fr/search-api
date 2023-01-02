1981bba2320bf9a329f316e7b43442008b8d5e4d
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-02 08:04:50 +0100

    feat: replace filters to ignore with filters to include in filter search by unite legale function

--- aio/aio-proxy/aio_proxy/search/filters/term_filters.py
+++ aio/aio-proxy/aio_proxy/search/filters/term_filters.py
@@ -1,11 +1,12 @@
 from aio_proxy.search.helpers.elastic_fields import get_elasticsearch_field_name
+from elasticsearch_dsl import Q
 
 
-def filter_search(search, filters_to_ignore: list, **params):
+def filter_term_search_unite_legale(search, filters_to_include: list, **params):
     """Use filters to reduce search results."""
     # params is the list of parameters (filters) provided in the request
     for param_name, param_value in params.items():
-        if param_value is not None and param_name not in filters_to_ignore:
+        if param_value is not None and param_name in filters_to_include:
             search = search.filter(
                 "term", **{get_elasticsearch_field_name(param_name): param_value}
             )

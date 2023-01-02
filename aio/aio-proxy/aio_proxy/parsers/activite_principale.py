2ebd3c6223304f37ff8cca9047097c0b464454c6
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-02 09:01:13 +0100

    feat: parse activite principale as list of strings instead of string

--- aio/aio-proxy/aio_proxy/parsers/activite_principale.py
+++ aio/aio-proxy/aio_proxy/parsers/activite_principale.py
@@ -1,26 +1,29 @@
-from typing import Optional
+from typing import List, Optional
 
 from aio_proxy.labels.helpers import codes_naf
 
 
-def validate_activite_principale(activite_principale_clean: str) -> Optional[str]:
-    """Check the validity of activite_principale.
+def validate_activite_principale(
+    list_activite_principale_clean: List[str],
+) -> Optional[List[str]]:
+    """Check the validity of list_activite_principale.
 
     Args:
-        activite_principale_clean(str, optional): activite_principale extracted and
-                                                cleaned.
+        list_activite_principale_clean(list(str), optional): activite_principale
+        extracted and cleaned.
 
     Returns:
         None if activite_principale_clean is None.
-        activite_principale_clean if valid.
+        list_activite_principale_clean if valid.
 
     Raises:
-        ValueError: if activite_principale_clean not valid.
+        ValueError: if one of the values in list_activite_principale_clean is not valid.
     """
-    if activite_principale_clean is None:
+    if list_activite_principale_clean is None:
         return None
-    if len(activite_principale_clean) != 6:
-        raise ValueError("Activité principale doit contenir 6 caractères.")
-    if activite_principale_clean not in codes_naf:
-        raise ValueError("Activité principale inconnue.")
-    return activite_principale_clean
+    for activite_principale in list_activite_principale_clean:
+        if len(activite_principale) != 6:
+            raise ValueError("Chaque activité principale doit contenir 6 caractères.")
+        if activite_principale not in codes_naf:
+            raise ValueError("Au moins une des activités principales est inconnue.")
+    return list_activite_principale_clean

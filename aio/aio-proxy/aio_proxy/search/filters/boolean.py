ecf8d204c641a4b017dbf50bf487be6c0de1d7fa
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-06 09:09:11 +0100

    refactor: define explicit variables for filter application conditions

--- aio/aio-proxy/aio_proxy/search/filters/boolean.py
+++ aio/aio-proxy/aio_proxy/search/filters/boolean.py
@@ -6,7 +6,11 @@ def filter_search_by_bool_fields_unite_legale(
     search, filters_to_include: list, **params
 ):
     for param_name, param_value in params.items():
-        if param_value is not None and param_name in filters_to_include:
+        is_param_defined_and_a_bool_filter = (
+            param_value is not None and param_name in filters_to_include
+        )
+        if is_param_defined_and_a_bool_filter:
+            # Check if bool filter value is True or False
             if param_value:
                 search = search.filter(
                     "exists", field=get_elasticsearch_field_name(param_name)

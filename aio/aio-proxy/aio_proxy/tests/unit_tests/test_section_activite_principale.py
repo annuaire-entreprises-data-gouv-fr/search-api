13d8a6c2a4c859344919a166ad77a0e8541859de
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-10 15:24:49 +0100

    tests: adapt section activite principale unit tests to list parsing

--- aio/aio-proxy/aio_proxy/tests/unit_tests/test_section_activite_principale.py
+++ aio/aio-proxy/aio_proxy/tests/unit_tests/test_section_activite_principale.py
@@ -6,7 +6,7 @@ from aio_proxy.parsers.section_activite_principale import (
 
 @pytest.mark.parametrize(
     "section_activite_principale, expected",
-    [("A", "A"), (None, None)],
+    [(["A", "A"]), ([], [])],
 )
 def test_validate_section_activite_principale(
     section_activite_principale: str, expected: str
@@ -14,9 +14,12 @@ def test_validate_section_activite_principale(
     assert validate_section_activite_principale(section_activite_principale) == expected
 
 
-@pytest.mark.parametrize("section_activite_principale", ["Z", "68.20B"])
+@pytest.mark.parametrize("section_activite_principale", [["Z", "68.20B"]])
 def test_validate_section_activite_principale_fail(
     section_activite_principale: str,
 ):
-    with pytest.raises(ValueError, match="Section d'activité principale non valide."):
+    with pytest.raises(
+        ValueError,
+        match="Au moins une " "section d'activité principale est non valide.",
+    ):
         validate_section_activite_principale(section_activite_principale)

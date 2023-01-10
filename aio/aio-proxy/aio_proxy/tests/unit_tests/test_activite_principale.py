f02fe53a2c20bc14209a22db5702f0e6a6746fd4
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-10 15:23:52 +0100

    tests: adapt code commune unit tests to list parsing

--- aio/aio-proxy/aio_proxy/tests/unit_tests/test_activite_principale.py
+++ aio/aio-proxy/aio_proxy/tests/unit_tests/test_activite_principale.py
@@ -4,21 +4,23 @@ from aio_proxy.parsers.activite_principale import validate_activite_principale
 
 @pytest.mark.parametrize(
     "activite_principale, expected",
-    [("62.01Z", "62.01Z"), ("27.33Z", "27.33Z"), (None, None)],
+    [(["62.01Z", "27.33Z"], ["62.01Z", "27.33Z"]), ([], [])],
 )
 def test_validate_activite_principale(activite_principale, expected):
     assert validate_activite_principale(activite_principale) == expected
 
 
-@pytest.mark.parametrize("activite_principale", ["11111", "2733Z"])
+@pytest.mark.parametrize("activite_principale", [["11111", "2733Z"]])
 def test_validate_activite_principale_fail_1(activite_principale: str):
     with pytest.raises(
-        ValueError, match="Activité principale doit contenir 6 " "caractères."
+        ValueError, match="Chaque activité principale doit contenir 6 caractères."
     ):
         validate_activite_principale(activite_principale)
 
 
-@pytest.mark.parametrize("activite_principale", ["27.33A"])
+@pytest.mark.parametrize("activite_principale", [["27.33A"]])
 def test_validate_activite_principale_fail_2(activite_principale: str):
-    with pytest.raises(ValueError, match="Activité principale inconnue."):
+    with pytest.raises(
+        ValueError, match="Au moins une des" " activités principales est inconnue."
+    ):
         validate_activite_principale(activite_principale)

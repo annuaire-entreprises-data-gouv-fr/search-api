cc7f35746dc650ca847de3d1fc3da9fa2350aa77
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-25 17:44:37 +0100

    feat: add parser for

---
+++ aio/aio-proxy/aio_proxy/parsers/matching_size.py
@@ -0,0 +1,27 @@
+from aio_proxy.decorators.value_exception import value_exception_handler
+
+
+@value_exception_handler(
+    error="Veuillez indiquer un nombre d'établissements connexes entier entre 1 et "
+          "100, par défaut 10."
+)
+def parse_and_validate_matching_size(request) -> int:
+    """Extract and Check the validity of page number.
+
+    Args:
+        request: HTTP request.
+
+    Returns:
+        matching_size(int) if valid.
+        default 10.
+
+    Raises:
+        ValueError: if matching_size is not integer.
+    """
+    matching_size = int(request.rel_url.query.get("limite_matching_etablissements", 10))
+    if matching_size <= 0 or matching_size > 100:
+        raise ValueError(
+            "Veuillez indiquer un nombre d'établissements connexes entier entre 1 et "
+            "100, par défaut 10."
+        )
+    return matching_size

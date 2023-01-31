3b6ec9f79f855a4e4a16b09607c4baa890525a75
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-31 17:59:08 +0100

    tests: add nature juridique unit tests

---
+++ aio/aio-proxy/aio_proxy/tests/unit_tests/test_nature_juridique.py
@@ -0,0 +1,25 @@
+import pytest
+from aio_proxy.parsers.nature_juridique import (
+    validate_nature_juridique,
+)
+
+
+@pytest.mark.parametrize(
+    "nature_juridique, expected",
+    [(["1000, 5658", "1000,5658"]), ([], [])],
+)
+def test_validate_nature_juridique(
+    nature_juridique: str, expected: str
+):
+    assert validate_nature_juridique(nature_juridique) == expected
+
+
+@pytest.mark.parametrize("nature_juridique", [["123!", "73, 7344"]])
+def test_validate_nature_juridique_fail(
+    nature_juridique: str,
+):
+    with pytest.raises(
+        ValueError,
+        match="Au moins une nature juridique est non valide.",
+    ):
+        validate_nature_juridique(nature_juridique)

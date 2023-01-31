5850d81523fe1d89266c197e35bd5ca31768d7d5
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-01-31 17:58:46 +0100

    feat: add nature juridique validation function

---
+++ aio/aio-proxy/aio_proxy/parsers/nature_juridique.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+
+from aio_proxy.labels.helpers import sections_codes_naf
+
+
+def validate_nature_juridique(
+    list_nature_juridique_clean: List[str],
+) -> Optional[List[str]]:
+    """Check the validity of list_section_activite_principale.
+
+    Args:
+        list_nature_juridique_clean(list(str), optional):
+        list_nature_juridique extracted and cleaned.
+
+    Returns:
+        None if list_nature_juridique_clean is None.
+        list_nature_juridique_clean if valid.
+
+    Raises:
+        ValueError: if one on the values in list_nature_juridique_clean is not valid.
+    """
+    if list_nature_juridique_clean is None:
+        return None
+    for nature_juridique in list_nature_juridique_clean:
+        if len(nature_juridique) != 4 or not nature_juridique.isdigit():
+            raise ValueError("Au moins une nature juridique est non valide.")
+    return list_nature_juridique_clean

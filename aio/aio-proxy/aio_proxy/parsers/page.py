cdca06f1f1548206eee37bdb09382b322a8009a1
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-03-24 16:04:35 +0100

    fix: return error if page number is higher than 1000

--- aio/aio-proxy/aio_proxy/parsers/page.py
+++ aio/aio-proxy/aio_proxy/parsers/page.py
@@ -1,9 +1,11 @@
 from aio_proxy.decorators.value_exception import value_exception_handler
 
+MAX_PAGE_VALUE = 1000
+MIN_PAGE_NUMBER = 0
+
 
 @value_exception_handler(
-    error="Veuillez indiquer un numéro de page entier supérieur ou égal à 1, "
-    "par défaut 1."
+    error="Veuillez indiquer un numéro de page entier entre 1 et 1000, par défaut 1."
 )
 def parse_and_validate_page(request) -> int:
     """Extract and Check the validity of page number.
@@ -16,12 +18,10 @@ def parse_and_validate_page(request) -> int:
         default 1.
 
     Raises:
-        ValueError: if page is not integer.
+        ValueError: if page is not integer, lower than 1 or higher than 1000.
     """
     page = int(request.rel_url.query.get("page", 1)) - 1  # default 1
-    if page <= -1:
-        raise ValueError(
-            "Veuillez indiquer un numéro de page entier supérieur ou "
-            "égal à 1, par défaut 1."
-        )
+    # 1000 is elasticsearch's default page limit
+    if page <= MIN_PAGE_NUMBER - 1 or page >= MAX_PAGE_VALUE:
+        raise ValueError
     return page

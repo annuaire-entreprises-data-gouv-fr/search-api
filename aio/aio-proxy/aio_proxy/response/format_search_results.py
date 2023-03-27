74cb1d2fa9ebbc7b4ab70bbe6c663fd54948eb9d
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-03-27 18:11:52 +0200

    fix: hide bilan financier from API response

--- aio/aio-proxy/aio_proxy/response/format_search_results.py
+++ aio/aio-proxy/aio_proxy/response/format_search_results.py
@@ -1,4 +1,3 @@
-from aio_proxy.response.formatters.bilan_financier import format_bilan
 from aio_proxy.response.formatters.bool import format_bool_field
 from aio_proxy.response.formatters.collectivite_territoriale import (
     format_collectivite_territoriale,
@@ -52,9 +51,7 @@ def format_search_results(results, include_etablissements=False):
             "matching_etablissements": format_etablissements_list(
                 get_field("matching_etablissements")
             ),
-            "bilan_financier": format_bilan(get_field("bilan_financier")),
             "complements": {
-                "bilan_renseigne": format_bool_field(get_field("bilan_financier")),
                 "collectivite_territoriale": format_collectivite_territoriale(
                     get_field("colter_code"),
                     get_field("colter_code_insee"),

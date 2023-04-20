e03cf72e81dc13ac903dbd095e179b08e8f16976
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-04-20 18:20:55 +0200

    feat: add validate categorie entreprise func

---
+++ aio/aio-proxy/aio_proxy/parsers/categorie_entreprise.py
@@ -0,0 +1,23 @@
+def validate_categorie_entreprise(list_categorie_entreprise_clean: list[str]):
+    """Check the validity of list_categorie_entreprise.
+
+    Args:
+        list_categorie_entreprise_clean(list(str), optional): categorie_entreprise
+        extracted and cleaned.
+
+    Returns:
+        None if activite_principale_clean is None.
+        list_activite_principale_clean if valid.
+
+    Raises:
+        ValueError: if one of the values in list_categorie_entreprise_clean is not valid
+    """
+    if list_categorie_entreprise_clean is None:
+        return None
+    for categorie_entreprise in list_categorie_entreprise_clean:
+        if categorie_entreprise not in ["GE", "PME", "ETI"]:
+            raise ValueError(
+                "Chaque catégorie d'entreprise doit prendre une de ces "
+                "valeurs 'GE', 'PME' ou 'ETI'."
+            )
+    return list_categorie_entreprise_clean

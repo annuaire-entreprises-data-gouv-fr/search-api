b069fb088cc90fc76771cf29154fc90d95954622
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-04-20 15:41:48 +0200

    feat: add non diffusible option to dirigeants object

--- aio/aio-proxy/aio_proxy/response/formatters/dirigeants.py
+++ aio/aio-proxy/aio_proxy/response/formatters/dirigeants.py
@@ -1,23 +1,32 @@
 from aio_proxy.response.helpers import get_value
 
 
-def format_dirigeants(dirigeants_pp=None, dirigeants_pm=None):
+def format_dirigeants(dirigeants_pp=None, dirigeants_pm=None, is_non_diffusible=False):
     dirigeants = []
     if dirigeants_pp:
         for dirigeant_pp in dirigeants_pp:
-            annee_de_naissance = (
-                get_value(dirigeant_pp, "date_naissance")[:4]
-                if get_value(dirigeant_pp, "date_naissance")
-                else None
-            )
+            if is_non_diffusible:
+                dirigeant = {
+                    "nom": "[NON-DIFFUSIBLE]",
+                    "prenoms": "[NON-DIFFUSIBLE]",
+                    "annee_de_naissance": "[NON-DIFFUSIBLE]",
+                    "qualite": get_value(dirigeant_pp, "qualite"),
+                    "type_dirigeant": "personne physique",
+                }
+            else:
+                annee_de_naissance = (
+                    get_value(dirigeant_pp, "date_naissance")[:4]
+                    if get_value(dirigeant_pp, "date_naissance")
+                    else None
+                )
 
-            dirigeant = {
-                "nom": get_value(dirigeant_pp, "nom"),
-                "prenoms": get_value(dirigeant_pp, "prenoms"),
-                "annee_de_naissance": annee_de_naissance,
-                "qualite": get_value(dirigeant_pp, "qualite"),
-                "type_dirigeant": "personne physique",
-            }
+                dirigeant = {
+                    "nom": get_value(dirigeant_pp, "nom"),
+                    "prenoms": get_value(dirigeant_pp, "prenoms"),
+                    "annee_de_naissance": annee_de_naissance,
+                    "qualite": get_value(dirigeant_pp, "qualite"),
+                    "type_dirigeant": "personne physique",
+                }
             dirigeants.append(dirigeant)
     if dirigeants_pm:
         for dirigeant_pm in dirigeants_pm:

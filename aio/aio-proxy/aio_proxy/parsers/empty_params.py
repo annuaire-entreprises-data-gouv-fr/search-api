06a688445ba09700745370d8eaac29db1eea7d77
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-05-12 14:16:03 +0200

    refactor: add SearchParams class to extract params

--- aio/aio-proxy/aio_proxy/parsers/empty_params.py
+++ aio/aio-proxy/aio_proxy/parsers/empty_params.py
@@ -7,7 +7,11 @@ def check_empty_params(parameters):
     # default value) raise value error
     empty_parameters = all(
         val is None
-        for val in [parameters[x] for x in parameters if x != "matching_size"]
+        for val in [
+            y
+            for x, y in vars(parameters).items()
+            if x not in ["page", "per_page", "matching_size"]
+        ]
     )
     if empty_parameters:
         raise ValueError

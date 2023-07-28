8d9d0ef57bf2aa72197febdddd10c3548ed91114
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-07-28 11:02:54 +0200

    refactor: replace list comprehension with for loop

--- aio/aio-proxy/aio_proxy/response/formatters/selected_fields.py
+++ aio/aio-proxy/aio_proxy/response/formatters/selected_fields.py
@@ -4,16 +4,10 @@ from aio_proxy.labels.helpers import valid_fields
 def select_fields_to_include(
     include_fields, etablissements, score, slug, response: dict
 ):
-    # Create a list of fields to remove from the result
-    fields_to_remove = [
-        field
-        for field in response
-        if field.upper() not in include_fields and field.upper() in valid_fields
-    ]
-
     # Remove the fields from the result
-    for field in fields_to_remove:
-        del response[field]
+    for field in valid_fields:
+        if field not in include_fields and field.lower() in response:
+            del response[field.lower()]
 
     # etablissements, score and slug are special fields used only by annuaire and a few
     # special users. These fields are always hidden, unless explicitly selected

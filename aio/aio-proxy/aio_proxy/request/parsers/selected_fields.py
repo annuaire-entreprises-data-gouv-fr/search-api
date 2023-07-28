f705f2bd016ace91fdfa1acfb98ea5f5968932e8
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-07-28 17:18:59 +0200

    feat: check if minimal is true before listing inclusion fields

---
+++ aio/aio-proxy/aio_proxy/request/parsers/selected_fields.py
@@ -0,0 +1,34 @@
+from aio_proxy.decorators.value_exception import value_exception_handler
+from aio_proxy.labels.helpers import valid_admin_fields, valid_fields
+
+
+def validate_selected_fields(list_fields_clean: list[str], admin=False):
+    if admin:
+        valid_fields_to_check = valid_admin_fields
+    else:
+        valid_fields_to_check = valid_fields
+    if list_fields_clean is None:
+        return []
+    for field in list_fields_clean:
+        if field not in valid_fields_to_check:
+            valid_fields_lowercase = [field.lower() for field in valid_fields_to_check]
+            raise ValueError(
+                f"Au moins un champ à inclure est non valide. "
+                f"Les champs valides : {valid_fields_lowercase}."
+            )
+    return list_fields_clean
+
+
+def should_include_etablissements(admin_fields):
+    if admin_fields and "ETABLISSEMENTS" in admin_fields:
+        return True
+    return None
+
+
+@value_exception_handler(
+    error="Veuillez indiquer si vous souhaitez une réponse minimale avec le filtre"
+    " 'minimal=True' avant de préciser les champs à inclure."
+)
+def validate_inclusion_fields(minimal: bool, include_fields: list[str]):
+    if include_fields and minimal is None or (include_fields and minimal is False):
+        raise ValueError

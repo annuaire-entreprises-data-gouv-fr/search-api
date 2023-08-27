eb84c68a39d7b18c6be3d4c10600ce158b6b6ed8
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-08-27 20:58:06 +0200

    feat: use pydantic classes to model unite legale

--- aio/aio-proxy/aio_proxy/response/unite_legale_model.py
+++ aio/aio-proxy/aio_proxy/response/unite_legale_model.py
@@ -1,143 +1,134 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
 
 
-@dataclass
-class UniteLegaleEtablissement:
-    activite_principale: str = None
-    activite_principale_registre_metier: str = None
+class UniteLegaleEtablissement(BaseModel):
+    activite_principale: str | None = None
+    activite_principale_registre_metier: str | None = None
     adresse: str = None
-    cedex: str = None
-    code_pays_etranger: str = None
-    code_postal: str = None
-    commune: str = None
-    complement_adresse: str = None
-    coordonnees: str = None
+    cedex: str | None = None
+    code_pays_etranger: str | None = None
+    code_postal: str | None = None
+    commune: str | None = None
+    complement_adresse: str | None = None
+    coordonnees: str | None = None
     date_creation: str = None
-    date_debut_activite: str = None
-    departement: str = None
-    distribution_speciale: str = None
-    est_siege: str = None
+    date_debut_activite: str | None = None
+    departement: str | None = None
+    distribution_speciale: str | None = None
+    est_siege: bool = None
     etat_administratif: str = None
-    geo_adresse: str = None
-    geo_id: str = None
-    indice_repetition: str = None
-    latitude: str = None
-    libelle_cedex: str = None
-    libelle_commune: str = None
-    libelle_commune_etranger: str = None
-    libelle_pays_etranger: str = None
-    libelle_voie: str = None
-    liste_enseignes: list = None
-    liste_finess: list = None
-    liste_id_bio: list = None
-    liste_idcc: list = None
-    liste_id_organisme_formation: list = None
-    liste_rge: list = None
-    liste_uai: list = None
-    longitude: str = None
-    nom_commercial: str = None
-    numero_voie: str = None
-    region: str = None
+    geo_adresse: str | None = None
+    geo_id: str | None = None
+    indice_repetition: str | None = None
+    latitude: str | None = None
+    libelle_cedex: str | None = None
+    libelle_commune: str | None = None
+    libelle_commune_etranger: str | None = None
+    libelle_pays_etranger: str | None = None
+    libelle_voie: str | None = None
+    liste_enseignes: list | None = None
+    liste_finess: list | None = None
+    liste_id_bio: list | None = None
+    liste_idcc: list | None = None
+    liste_id_organisme_formation: list | None = None
+    liste_rge: list | None = None
+    liste_uai: list | None = None
+    longitude: str | None = None
+    nom_commercial: str | None = None
+    numero_voie: str | None = None
+    region: str | None = None
     siret: str = None
-    tranche_effectif_salarie: str = None
-    type_voie: str = None
+    tranche_effectif_salarie: str | None = None
+    type_voie: str | None = None
 
 
-@dataclass
-class UniteLegaleElu:
-    nom: str = None
-    prenoms: str = None
-    annee_de_naissance: str = None
-    fonction: str = None
-    sexe: str = None
+class UniteLegaleElu(BaseModel):
+    nom: str | None = None
+    prenoms: str | None = None
+    annee_de_naissance: str | None = None
+    fonction: str | None = None
+    sexe: str | None = None
 
 
-@dataclass
-class UniteLegaleDirigeantsPM:
+class UniteLegaleDirigeantsPM(BaseModel):
     siren: str = None
-    denomination: str = None
-    sigle: str = None
-    qualite: str = None
-    type_dirigeant: str = None
+    denomination: str | None = None
+    sigle: str | None = None
+    qualite: str | None = None
+    type_dirigeant: str = "personne morale"
 
 
-@dataclass
-class UniteLegaleDirigeantsPP:
-    nom: str = None
-    prenoms: str = None
-    annee_de_naissance: str = None
-    qualite: str = None
+class UniteLegaleDirigeantsPP(BaseModel):
+    nom: str | None = None
+    prenoms: str | None = None
+    annee_de_naissance: str | None = None
+    qualite: str | None = None
     type_dirigeant: str = "personne physique"
 
 
-@dataclass
-class UniteLegaleDirigeant:
-    dirigeants_pm: list[UniteLegaleDirigeantsPM]
-    dirigeants_pp: list[UniteLegaleDirigeantsPP]
+class UniteLegaleDirigeant(BaseModel):
+    dirigeants_pm: list[UniteLegaleDirigeantsPM] | None
+    dirigeants_pp: list[UniteLegaleDirigeantsPP] | None
 
 
-@dataclass
-class UniteLegaleFinances:
-    annee_cloture_exercice: int = None
-    ca: int = None
-    resultat_net: int = None
+class UniteLegaleFinances(BaseModel):
+    annee_cloture_exercice: str | None = None
+    ca: int | None = None
+    resultat_net: int | None = None
 
 
-@dataclass
-class UniteLegaleCollectiviteTerritoriale:
+class UniteLegaleCollectiviteTerritoriale(BaseModel):
     code: str = None
     code_insee: str = None
     elus: list[UniteLegaleElu] = None
-    niveau: str = None
+    niveau: str | None = None
 
 
-@dataclass
-class UniteLegaleComplements:
-    collectivite_territoriale: UniteLegaleCollectiviteTerritoriale
-    convention_collective_renseignee: str = None
-    egapro_renseignee: str = None
-    est_bio: str = None
-    est_entrepreneur_individuel: str = None
-    est_entrepreneur_spectacle: str = None
-    est_ess: str = None
-    est_finess: str = None
-    est_organisme_formation: str = None
-    est_qualiopi: str = None
-    liste_id_organisme_formation: str = None
-    est_rge: str = None
-    est_service_public: str = None
-    est_societe_mission: str = None
-    est_uai: str = None
-    identifiant_association: str = None
-    statut_entrepreneur_spectacle: str = None
+class UniteLegaleComplements(BaseModel):
+    collectivite_territoriale: UniteLegaleCollectiviteTerritoriale | None = None
+    convention_collective_renseignee: bool = None
+    egapro_renseignee: bool = None
+    est_bio: bool = None
+    est_entrepreneur_individuel: bool = None
+    est_entrepreneur_spectacle: bool = None
+    est_ess: bool = None
+    est_finess: bool = None
+    est_organisme_formation: bool = None
+    est_qualiopi: bool = None
+    liste_id_organisme_formation: list | None = None
+    est_rge: bool = None
+    est_service_public: bool = None
+    est_societe_mission: bool = None
+    est_uai: bool = None
+    identifiant_association: str | None = None
+    statut_entrepreneur_spectacle: str | None = None
 
 
-@dataclass
-class UniteLegaleResponse:
-    """Class for modelling unite légale response"""
-
+class UniteLegaleResponse(BaseModel):
     siren: str = None
-    nom_complet: str = None
-    nom_raison_sociale: str = None
-    sigle: str = None
+    nom_complet: str | None = None
+    nom_raison_sociale: str | None = None
+    sigle: str | None = None
     nombre_etablissements: int = None
     nombre_etablissements_ouverts: int = None
     siege: UniteLegaleEtablissement = None
-    activite_principale: str = None
-    categorie_entreprise: str = None
-    annee_categorie_entreprise: str = None
-    date_creation: str = None
-    date_mise_a_jour: str = None
-    dirigeants: list[UniteLegaleDirigeantsPP] | list[UniteLegaleDirigeantsPM] = None
+    activite_principale: str | None = None
+    categorie_entreprise: str | None = None
+    annee_categorie_entreprise: str | None = None
+    date_creation: str | None = None
+    date_mise_a_jour: str | None = None
+    dirigeants: (
+        list[UniteLegaleDirigeantsPP] | list[UniteLegaleDirigeantsPM] | None
+    ) = None
     etat_administratif: str = None
-    nature_juridique: str = None
-    section_activite_principale: str = None
-    tranche_effectif_salarie: str = None
-    annee_tranche_effectif_salarie: str = None
+    nature_juridique: str | None = None
+    section_activite_principale: str | None = None
+    tranche_effectif_salarie: str | None = None
+    annee_tranche_effectif_salarie: str | None = None
     statut_diffusion: str = None
-    matching_etablissements: str = None
-    etablissements: list[UniteLegaleEtablissement] = None
-    finances: list[UniteLegaleFinances] = None
+    matching_etablissements: list[UniteLegaleEtablissement] | None = None
+    etablissements: UniteLegaleEtablissement | None = None
+    finances: dict[str, UniteLegaleFinances] | None = None
     complements: UniteLegaleComplements = None
     score: float = None
     slug: str = None

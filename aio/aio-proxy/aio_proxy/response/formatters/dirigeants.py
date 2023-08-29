c9aafcd76c5b968fdc00a3697db2c20e50fd6d0e
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-08-29 13:21:16 +0200

    refactor: group non diffusibilite logic in one function call

--- aio/aio-proxy/aio_proxy/response/formatters/dirigeants.py
+++ aio/aio-proxy/aio_proxy/response/formatters/dirigeants.py
@@ -9,29 +9,19 @@ def format_dirigeants(dirigeants_pp=None, dirigeants_pm=None, is_non_diffusible=
     dirigeants = []
     if dirigeants_pp:
         for dirigeant_pp in dirigeants_pp:
-            if is_non_diffusible:
-                dirigeant = UniteLegaleDirigeantsPP(
-                    nom="[NON-DIFFUSIBLE]",
-                    prenoms="[NON-DIFFUSIBLE]",
-                    annee_de_naissance="[NON-DIFFUSIBLE]",
-                    qualite=get_value(dirigeant_pp, "qualite"),
-                    type_dirigeant="personne physique",
-                )
-            else:
-                annee_de_naissance = (
-                    get_value(dirigeant_pp, "date_naissance")[:4]
-                    if get_value(dirigeant_pp, "date_naissance")
-                    else None
-                )
-
-                dirigeant = UniteLegaleDirigeantsPP(
-                    nom=get_value(dirigeant_pp, "nom"),
-                    prenoms=get_value(dirigeant_pp, "prenoms"),
-                    annee_de_naissance=annee_de_naissance,
-                    qualite=get_value(dirigeant_pp, "qualite"),
-                    type_dirigeant="personne physique",
-                )
-            dirigeants.append(dirigeant)
+            annee_de_naissance = (
+                get_value(dirigeant_pp, "date_naissance")[:4]
+                if get_value(dirigeant_pp, "date_naissance")
+                else None
+            )
+            dirigeant = UniteLegaleDirigeantsPP(
+                nom=get_value(dirigeant_pp, "nom"),
+                prenoms=get_value(dirigeant_pp, "prenoms"),
+                annee_de_naissance=annee_de_naissance,
+                qualite=get_value(dirigeant_pp, "qualite"),
+                type_dirigeant="personne physique",
+            )
+        dirigeants.append(dirigeant)
     if dirigeants_pm:
         for dirigeant_pm in dirigeants_pm:
             sigle = (
--- aio/aio-proxy/aio_proxy/response/formatters/non_diffusible.py
+++ aio/aio-proxy/aio_proxy/response/formatters/non_diffusible.py
@@ -7,4 +7,57 @@ def hide_non_diffusible_fields(result_formatted):
     for field in result_formatted.keys():
         if field in hidden_fields:
             result_formatted[field] = "[NON-DIFFUSIBLE]"
+
+        if field == "siege":
+            result_formatted["siege"] = hide_non_diffusible_etablissement_fields(
+                result_formatted["siege"]
+            )
+
+        if field == "dirigeants":
+            result_formatted["dirigeants"] = hide_non_diffusible_dirirgeants_fields(
+                result_formatted["dirigeants"]
+            )
+
+        if field == "matching_etablissements":
+            for matching_etablissement in result_formatted["matching_etablissements"]:
+                matching_etablissement = hide_non_diffusible_etablissement_fields(
+                    matching_etablissement
+                )
+
+        if field == "etablissements":
+            for etablissement in result_formatted["etablissements"]:
+                etablissement = hide_non_diffusible_etablissement_fields(etablissement)
+
     return result_formatted
+
+
+def hide_non_diffusible_dirirgeants_fields(dirigeants):
+    for dirigeant in dirigeants:
+        if dirigeant["type_dirigeant"] == "personne physique":
+            dirigeant["nom"] = "[NON-DIFFUSIBLE]"
+            dirigeant["prenoms"] = "[NON-DIFFUSIBLE]"
+            dirigeant["annee_de_naissance"] = "[NON-DIFFUSIBLE]"
+    return dirigeants
+
+
+def hide_non_diffusible_etablissement_fields(etablissement):
+    # in order to keep `liste_enseignes` as an array of "NON-DIFFUSIBLE" strings
+    if etablissement["liste_enseignes"]:
+        etablissement["liste_enseignes"] = [
+            "[NON-DIFFUSIBLE]" for enseigne in etablissement["liste_enseignes"]
+        ]
+    non_diffusible_fields = [
+        "cedex",
+        "code_postal",
+        "complement_adresse",
+        "distribution_speciale",
+        "indice_repetition",
+        "libelle_cedex",
+        "libelle_voie",
+        "nom_commercial",
+        "numero_voie",
+        "type_voie",
+    ]
+    for field in non_diffusible_fields:
+        etablissement[field] = "[NON-DIFFUSIBLE]"
+    return etablissement

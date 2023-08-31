a2bd9160d80a892fd93dbeddfa9f5e871fe3fd40
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-08-31 21:21:04 +0200

    feat: add association response to possible response model

--- aio/aio-proxy/aio_proxy/response/response_model.py
+++ aio/aio-proxy/aio_proxy/response/response_model.py
@@ -1,9 +1,10 @@
+from aio_proxy.response.association_model import AssociationResponse
 from aio_proxy.response.unite_legale_model import UniteLegaleResponse
 from pydantic import BaseModel
 
 
 class ResponseModel(BaseModel):
-    results: list[UniteLegaleResponse] | None = None
+    results: list[UniteLegaleResponse | AssociationResponse] | None = None
     total_results: int = None
     page: int = None
     per_page: int = None

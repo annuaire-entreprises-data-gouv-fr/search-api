8dfe0552f66a269cbf49f604e80bc1f0bc288083
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-09-11 11:04:16 +0200

    refactor: use pydantic class to model search params

--- aio/aio-proxy/aio_proxy/request/search_params_model.py
+++ aio/aio-proxy/aio_proxy/request/search_params_model.py
@@ -1,56 +1,56 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
 
 
-@dataclass
-class SearchParams:
-    """Class for modelling search parameters"""
+class SearchParams(BaseModel):
+    """Class for modeling search parameters"""
 
-    page: int = None
-    per_page: int = None
-    terms: str = None
-    activite_principale_unite_legale: list = None
-    categorie_entreprise: list = None
-    commune: list = None
-    code_postal: list = None
-    departement: list = None
-    region: list = None
-    est_entrepreneur_individuel: str = None
-    section_activite_principale: list = None
-    tranche_effectif_salarie_unite_legale: list = None
-    convention_collective_renseignee: str = None
-    egapro_renseignee: str = None
-    est_bio: str = None
-    est_finess: str = None
-    est_uai: str = None
-    est_collectivite_territoriale: str = None
-    est_entrepreneur_spectacle: str = None
-    est_association: str = None
-    est_organisme_formation: str = None
-    est_qualiopi: str = None
-    est_rge: str = None
-    est_service_public: str = None
-    est_societe_mission: str = None
-    economie_sociale_solidaire_unite_legale: str = None
-    id_convention_collective: str = None
-    id_finess: str = None
-    id_uai: str = None
-    code_collectivite_territoriale: str = None
-    id_rge: str = None
-    nom_personne: str = None
-    prenoms_personne: str = None
-    min_date_naiss_personne: str = None
-    max_date_naiss_personne: str = None
-    ca_min: str = None
-    ca_max: str = None
-    resultat_net_min: str = None
-    resultat_net_max: str = None
-    type_personne: str = None
-    etat_administratif_unite_legale: str = None
-    nature_juridique_unite_legale: list = None
-    matching_size: str = None
-    lat: float = None
-    lon: float = None
-    radius: float = None
-    minimal: bool = None
-    include: list = None
-    include_admin: list = None
+    page: int = 1
+    per_page: int = 10
+    terms: str | None = None
+    activite_principale_unite_legale: list | None = None
+    categorie_entreprise: list | None = None
+    commune: list | None = None
+    code_postal: list | None = None
+    departement: list | None = None
+    region: list | None = None
+    est_entrepreneur_individuel: bool | None = None
+    section_activite_principale: list | None = None
+    tranche_effectif_salarie_unite_legale: list | None = None
+    convention_collective_renseignee: bool | None = None
+    egapro_renseignee: bool | None = None
+    est_bio: bool | None = None
+    est_finess: bool | None = None
+    est_uai: bool | None = None
+    est_collectivite_territoriale: bool | None = None
+    est_entrepreneur_spectacle: bool | None = None
+    est_association: bool | None = None
+    est_organisme_formation: bool | None = None
+    est_qualiopi: bool | None = None
+    est_rge: bool | None = None
+    est_service_public: bool | None = None
+    est_societe_mission: bool | None = None
+    economie_sociale_solidaire_unite_legale: str | None = None
+    id_convention_collective: str | None = None
+    id_finess: str | None = None
+    id_uai: str | None = None
+    code_collectivite_territoriale: str | None = None
+    id_rge: str | None = None
+    nom_personne: str | None = None
+    prenoms_personne: str | None = None
+    min_date_naiss_personne: str | None = None
+    max_date_naiss_personne: str | None = None
+    ca_min: str | None = None
+    ca_max: str | None = None
+    resultat_net_min: float | None = None
+    resultat_net_max: float | None = None
+    type_personne: str | None = None
+    etat_administratif_unite_legale: str | None = None
+    nature_juridique_unite_legale: list | None = None
+    matching_size: int | None = 10
+    lat: float | None = None
+    lon: float | None = None
+    radius: float | None = None
+    minimal: bool | None = False
+    include: list | None = None
+    include_admin: list | None = None
+

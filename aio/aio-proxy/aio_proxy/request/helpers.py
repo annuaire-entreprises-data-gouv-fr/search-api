1ed227ca2df36c0c2627319e916d05413470f6c3
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-09-14 21:21:31 +0200

    fix: add 400 error message when page*per_page is bigger than 10 000

--- aio/aio-proxy/aio_proxy/request/helpers.py
+++ aio/aio-proxy/aio_proxy/request/helpers.py
@@ -8,3 +8,17 @@ def validate_date_range(min_date=None, max_date=None):
     if min_date and max_date:
         if max_date < min_date:
             raise ValueError
+
+
+MAX_RESULTS = 10000
+
+
+@value_exception_handler(
+    error="Le nombre total de résultats est restreint à 10 000. Pour garantir cela, "
+    "le produit du numéro de page (par défaut, page = 1) et du nombre de résultats "
+    "par page (par défaut, per_page = 10), c'est-à-dire `page * per_page`, ne doit pas "
+    "excéder 10 000."
+)
+def validate_results_window(page=1, per_page=10):
+    if page * per_page > MAX_RESULTS:
+        raise ValueError

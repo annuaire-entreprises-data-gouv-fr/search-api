a32f43e08f5907636e63fc4081745a56902e377f
HAEKADI <hajar.aitelkadi.o@gmail.com>
2023-11-15 19:47:39 +0100

    feat: update diriegeants fields

--- aio/aio-proxy/aio_proxy/response/unite_legale_model.py
+++ aio/aio-proxy/aio_proxy/response/unite_legale_model.py
@@ -57,7 +57,6 @@ class Elu(BaseModel):
 class DirigeantsPM(BaseModel):
     siren: str | None = None
     denomination: str | None = None
-    sigle: str | None = None
     qualite: str | None = None
     type_dirigeant: Literal["personne morale"]
 
@@ -67,6 +66,7 @@ class DirigeantsPP(BaseModel):
     prenoms: str | None = None
     annee_de_naissance: str | None = None
     qualite: str | None = None
+    nationalite: str | None = None
     type_dirigeant: Literal["personne physique"]
 
 

43ad77408889ca9d210168806e3d0aa54306794a
HAEKADI <hajar.aitelkadi@proton.me>
2023-11-27 18:42:34 +0100

    feat: update fields value for page_etablissements

--- aio/aio-proxy/aio_proxy/request/field_values.py
+++ aio/aio-proxy/aio_proxy/request/field_values.py
@@ -23,6 +23,12 @@ VALID_ADMIN_FIELDS_TO_SELECT = [
 
 NUMERIC_FIELD_LIMITS = {
     "page": {"min": 1, "max": 1000, "default": 1, "alias": "page"},
+    "page_etablissements": {
+        "min": 1,
+        "max": 1000,
+        "default": 1,
+        "alias": "page_etablissements",
+    },
     "per_page": {"min": 1, "max": 25, "default": 10, "alias": "per_page"},
     "matching_size": {
         "min": 1,

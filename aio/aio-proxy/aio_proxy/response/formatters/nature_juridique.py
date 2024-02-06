1bd8e8eccaecea49a2fdc6651bf011eb9dfdc5c3
HAEKADI <hajar.aitelkadi.o@gmail.com>
2024-02-06 14:52:45 +0100

    feat: add nature juridique response formatter

---
+++ aio/aio-proxy/aio_proxy/response/formatters/nature_juridique.py
@@ -0,0 +1,8 @@
+from aio_proxy.labels.helpers import NATURES_JURIDIQUES
+
+
+def format_nature_juridique(nature_juridique):
+    if nature_juridique not in NATURES_JURIDIQUES:
+        return None
+    else:
+        return nature_juridique

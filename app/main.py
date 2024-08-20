682ac8d7c61116dd3458de4e6434a98539f67cc6
HAEKADI <hajar.aitelkadi@proton.me>
2024-08-20 13:53:49 +0200

    docs: add openapi config

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import os
-import pathlib
 
+import yaml
 from dotenv import load_dotenv
 from elasticsearch_dsl import connections
 from fastapi import FastAPI
@@ -16,6 +16,11 @@ ELASTIC_USER = os.getenv("ELASTIC_USER")
 ELASTIC_URL = os.getenv("ELASTIC_URL")
 DSN_SENTRY = os.getenv("DSN_SENTRY")
 
+load_dotenv()
+
+ENV = os.getenv("ENV")
+open_api_path = "doc/open-api.yml"
+
 # Connect to Elasticsearch
 connections.create_connection(
     hosts=[ELASTIC_URL],
@@ -23,20 +28,28 @@ connections.create_connection(
     retry_on_timeout=True,
 )
 
-app = FastAPI()
-
-load_dotenv()
-
-BASE_DIR = pathlib.Path(__file__).parent.parent
-open_api_path = BASE_DIR / "aio_proxy" / "doc" / "open-api.yml"
-ENV = os.getenv("ENV")
 
 app = FastAPI(
     title="API Recherche d'entreprises",
-    version="2.0.0",
-    docs_url="/docs/",
-    redoc_url="/redoc/",
+    version="1.0.0",
+    docs_url=None,
+    redoc_url="/docs/",
 )
+
+
+# Load OpenAPI YAML file
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    with open(open_api_path) as file:
+        openapi_schema = yaml.safe_load(file)
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+# Assign the custom OpenAPI method to the FastAPI app
+app.openapi = custom_openapi
+
 
 # Include the router
 app.include_router(router)

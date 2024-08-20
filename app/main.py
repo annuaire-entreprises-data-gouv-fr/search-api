be114ec90d215131e18fe6d0bc5b37768c967b5b
HAEKADI <hajar.aitelkadi@proton.me>
2024-08-20 14:20:19 +0200

    feat: add elasticapm integration

--- app/main.py
+++ app/main.py
@@ -3,15 +3,16 @@ import os
 
 import yaml
 from dotenv import load_dotenv
+from elasticapm.contrib.starlette import ElasticAPM, make_apm_client
 from elasticsearch_dsl import connections
 from fastapi import FastAPI
 
+from app.response.helpers import APM_URL, CURRENT_ENV
 from app.router import router
 
 load_dotenv()
 
 # Get env variables
-ENV = os.getenv("ENV")
 ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD")
 ELASTIC_USER = os.getenv("ELASTIC_USER")
 ELASTIC_URL = os.getenv("ELASTIC_URL")
@@ -19,7 +20,6 @@ DSN_SENTRY = os.getenv("DSN_SENTRY")
 
 load_dotenv()
 
-ENV = os.getenv("ENV")
 open_api_path = "doc/open-api.yml"
 
 logging.basicConfig(level=logging.INFO)
@@ -47,11 +47,21 @@ def custom_openapi():
     with open(open_api_path) as file:
         openapi_schema = yaml.safe_load(file)
     app.openapi_schema = openapi_schema
-    return app.openapi_schema
+    return app.openapi_schemab
 
 
 # Assign the custom OpenAPI method to the FastAPI app
 app.openapi = custom_openapi
+
+# Elastic APM Integration
+if CURRENT_ENV != "dev":
+    apm_config = {
+        "SERVICE_NAME": "SEARCH APM",
+        "SERVER_URL": APM_URL,
+        "ENVIRONMENT": CURRENT_ENV,
+    }
+    apm_client = make_apm_client(apm_config)
+    app.add_middleware(ElasticAPM, client=apm_client)
 
 
 # Include the router

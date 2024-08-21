56ed90bc32bbb4754701c1e3e5686ef12774f4a9
HAEKADI <hajar.aitelkadi@proton.me>
2024-08-21 16:19:17 +0200

    feat: refactor main file

--- app/main.py
+++ app/main.py
@@ -1,31 +1,21 @@
-import logging
-import os
-
-import sentry_sdk
 import yaml
-from dotenv import load_dotenv
 from elasticapm.contrib.starlette import ElasticAPM, make_apm_client
 from elasticsearch_dsl import connections
 from fastapi import FastAPI
-from sentry_sdk.integrations.fastapi import FastApiIntegration
-from sentry_sdk.integrations.logging import LoggingIntegration
 
-from app.response.helpers import APM_URL, CURRENT_ENV
+from app.config import (
+    APM_CONFIG,
+    CURRENT_ENV,
+    ELASTIC_PASSWORD,
+    ELASTIC_URL,
+    ELASTIC_USER,
+    OPEN_API_PATH,
+)
+from app.logging import setup_logging, setup_sentry
 from app.router import router
 
-load_dotenv()
-
-# Get env variables
-ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD")
-ELASTIC_USER = os.getenv("ELASTIC_USER")
-ELASTIC_URL = os.getenv("ELASTIC_URL")
-DSN_SENTRY = os.getenv("DSN_SENTRY")
-
-load_dotenv()
-
-open_api_path = "doc/open-api.yml"
-
-logging.basicConfig(level=logging.INFO)
+# Setup logging
+setup_logging()
 
 # Connect to Elasticsearch
 connections.create_connection(
@@ -33,7 +23,6 @@ connections.create_connection(
     http_auth=(ELASTIC_USER, ELASTIC_PASSWORD),
     retry_on_timeout=True,
 )
-
 
 app = FastAPI(
     title="API Recherche d'entreprises",
@@ -47,40 +36,19 @@ app = FastAPI(
 def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
-    with open(open_api_path) as file:
+    with open(OPEN_API_PATH) as file:
         openapi_schema = yaml.safe_load(file)
     app.openapi_schema = openapi_schema
-    return app.openapi_schemab
+    return app.openapi_schema
 
 
-# Assign the custom OpenAPI method to the FastAPI app
 app.openapi = custom_openapi
 
-# Elastic APM Integration
-if CURRENT_ENV != "dev":
-    apm_config = {
-        "SERVICE_NAME": "SEARCH APM",
-        "SERVER_URL": APM_URL,
-        "ENVIRONMENT": CURRENT_ENV,
-    }
-    apm_client = make_apm_client(apm_config)
-    app.add_middleware(ElasticAPM, client=apm_client)
-
-
-# Sentry Integration for Production
+# Sentry Integration and Elastic APM Integration for Production
 if CURRENT_ENV == "prod":
-    sentry_logging = LoggingIntegration(
-        level=logging.INFO,  # Capture info and above as breadcrumbs
-        event_level=logging.WARNING,  # Send warnings as events
-    )
-    sentry_sdk.init(
-        dsn=DSN_SENTRY,
-        integrations=[
-            FastApiIntegration(),
-            sentry_logging,
-        ],
-        traces_sample_rate=0.01,  # Log 1% of transactions for performance monitoring
-    )
+    apm_client = make_apm_client(APM_CONFIG)
+    app.add_middleware(ElasticAPM, client=apm_client)
+    setup_sentry()
 
 # Include the router
 app.include_router(router)

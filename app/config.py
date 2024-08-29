47be31c14b641f1251b218c76a188860e1d96d0f
HAEKADI <hajar.aitelkadi@proton.me>
2024-08-29 11:46:59 +0200

    refactor: update config

--- app/config.py
+++ app/config.py
@@ -10,13 +10,10 @@ ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD")
 ELASTIC_USER = os.getenv("ELASTIC_USER")
 ELASTIC_URL = os.getenv("ELASTIC_URL")
 DSN_SENTRY = os.getenv("DSN_SENTRY")
-
-# Other configuration constants
 CURRENT_ENV = os.getenv("CURRENT_ENV", "dev")
 APM_URL = os.getenv("APM_URL")
 BASE_DIR = pathlib.Path(__file__).parent
 OPEN_API_PATH = BASE_DIR / "doc" / "open-api.yml"
-
 
 # Elastic APM configuration
 APM_CONFIG = {

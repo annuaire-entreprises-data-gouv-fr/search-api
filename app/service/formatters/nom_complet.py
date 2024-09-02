cf8ae284859a0046393661394520da2ddfa67a52
HAEKADI <hajar.aitelkadi@proton.me>
2024-09-02 18:58:24 +0200

    feat: use nom commercial in ul nom complet

--- app/service/formatters/nom_complet.py
+++ app/service/formatters/nom_complet.py
@@ -1,24 +1,40 @@
+def get_nom_commercial(siege):
+    return siege.get("nom_commercial", None) if siege else None
+
+
 def format_nom_complet(
     nom_complet,
     sigle=None,
+    nom_commercial_siege=None,
     denomination_usuelle_1=None,
     denomination_usuelle_2=None,
     denomination_usuelle_3=None,
 ):
     """Add `denomination usuelle` fields and `sigle` to `nom_complet`."""
-    all_denomination_usuelle = ""
-    for item in [
-        denomination_usuelle_1,
-        denomination_usuelle_2,
-        denomination_usuelle_3,
-    ]:
-        if item:
-            all_denomination_usuelle += f"{item} "
-    if all_denomination_usuelle:
-        nom_complet = f"{nom_complet} ({all_denomination_usuelle.strip()})"
+    if not nom_complet:
+        return None
+
+    # Handle denomination usuelle
+    if nom_commercial_siege:
+        denomination = nom_commercial_siege
+    else:
+        denomination = " ".join(
+            filter(
+                None,
+                [
+                    denomination_usuelle_1,
+                    denomination_usuelle_2,
+                    denomination_usuelle_3,
+                ],
+            )
+        )
+
+    # Add denomination to nom_complet if it exists
+    if denomination:
+        nom_complet += f" ({denomination.strip()})"
+
+    # Add sigle if it exists
     if sigle:
-        nom_complet = f"{nom_complet} ({sigle})"
-    if nom_complet:
-        return nom_complet.upper()
-    # if nom_complet is null
-    return None
+        nom_complet += f" ({sigle})"
+
+    return nom_complet.upper()

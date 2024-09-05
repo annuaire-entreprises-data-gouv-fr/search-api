bbcae275b7da3ee697f5a3b006f51833775bb120
HAEKADI <hajar.aitelkadi@proton.me>
2024-09-05 12:05:48 +0200

    feat: use pydantic settings class for config

--- app/config.py
+++ app/config.py
@@ -1,23 +1,53 @@
-import os
-import pathlib
+from pathlib import Path
+from typing import Any, Dict
 
-from dotenv import load_dotenv
+from pydantic import AnyHttpUrl, Field, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv()
 
-# Environment variables
-ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD")
-ELASTIC_USER = os.getenv("ELASTIC_USER")
-ELASTIC_URL = os.getenv("ELASTIC_URL")
-DSN_SENTRY = os.getenv("DSN_SENTRY")
-CURRENT_ENV = os.getenv("ENV", "dev")
-APM_URL = os.getenv("APM_URL")
-BASE_DIR = pathlib.Path(__file__).parent
-OPEN_API_PATH = BASE_DIR / "doc" / "open-api.yml"
+class ElasticConfig(BaseSettings):
+    password: SecretStr = Field(...)
+    user: str = Field(...)
+    url: AnyHttpUrl = Field(...)
 
-# Elastic APM configuration
-APM_CONFIG = {
-    "SERVICE_NAME": "SEARCH APM",
-    "SERVER_URL": APM_URL,
-    "ENVIRONMENT": CURRENT_ENV,
-}
+
+class SentryConfig(BaseSettings):
+    dsn: str = Field(...)
+
+
+class APMConfig(BaseSettings):
+    url: AnyHttpUrl = Field(...)
+    service_name: str = Field("SEARCH APM")
+
+
+class DocsConfig(BaseSettings):
+    path: Path = Field(
+        default_factory=lambda: Path(__file__).parent / "doc" / "open-api.yml"
+    )
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+        env_nested_delimiter="_",
+    )
+
+    elastic: ElasticConfig
+    sentry: SentryConfig
+    apm: APMConfig
+    openapi: DocsConfig = DocsConfig()
+    env: str = Field(...)
+
+    @property
+    def apm_config(self) -> Dict[str, Any]:
+        return {
+            "SERVICE_NAME": self.apm.service_name,
+            "SERVER_URL": self.apm.url,
+            "ENVIRONMENT": self.env,
+        }
+
+
+settings = Settings()

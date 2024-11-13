5d6bdec00b108aa9a5e87c8611c1eb3758ede93f
HAEKADI <hajar.aitelkadi@proton.me>
2024-11-13 16:42:40 +0100

    fix(security): remove permissive regular expression

--- app/elastic/parsers/siren.py
+++ app/elastic/parsers/siren.py
@@ -1,14 +1,13 @@
-import re
+SIREN_LENGTH = 9  # Constant for the length of a SIREN number
 
 
 def is_siren(query_string: str) -> bool:
     """
     Check if string is siren (composed of 9 digits).
     """
-    if query_string is None:
+    if query_string is None or not isinstance(query_string, str):
         return False
     clean_query_string = query_string.replace(" ", "")
-    siren_valides = r"\b\d{9}\b"
-    if re.search(siren_valides, clean_query_string):
-        return True
-    return False
+    if len(clean_query_string) != SIREN_LENGTH or not clean_query_string.isdigit():
+        return False
+    return True

e7b9d89aedcf5bd9e55b079e8282e825afd0eb14
HAEKADI <hajar.aitelkadi@proton.me>
2024-11-13 16:54:15 +0100

    fix(security): remove permissive regular expression in siret parser

--- app/elastic/parsers/siret.py
+++ app/elastic/parsers/siret.py
@@ -1,14 +1,17 @@
-import re
+SIRET_LENGTH = 14  # Constant for the length of a SIRET number
 
 
 def is_siret(query_string: str) -> bool:
     """
     Check if string is siret (composed of 14 digits).
     """
-    if query_string is None:
+    if query_string is None or not isinstance(query_string, str):
         return False
+
     clean_query_string = query_string.replace(" ", "")
-    siret_valides = r"\b\d{14}\b"
-    if re.search(siret_valides, clean_query_string):
+
+    # Validate length and digit-only condition
+    if len(clean_query_string) == SIRET_LENGTH and clean_query_string.isdigit():
         return True
+
     return False

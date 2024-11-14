7f803dcbbc34b2b3e12d80eb8a65262a4c71fc9b
HAEKADI <hajar.aitelkadi@proton.me>
2024-11-14 10:42:24 +0100

    refactor: use regular expression to validate siren and siret

--- app/elastic/parsers/siren.py
+++ app/elastic/parsers/siren.py
@@ -1,13 +1,13 @@
-SIREN_LENGTH = 9  # Constant for the length of a SIREN number
+import re
 
 
 def is_siren(query_string: str) -> bool:
     """
-    Check if string is siren (composed of 9 digits).
+    Check if string is siren (composed of exactly 9 digits).
     """
+
     if query_string is None or not isinstance(query_string, str):
         return False
     clean_query_string = query_string.replace(" ", "")
-    if len(clean_query_string) != SIREN_LENGTH or not clean_query_string.isdigit():
-        return False
-    return True
+    # Using regular expression to check for exactly 9 digits
+    return bool(re.fullmatch(r"^\d{9}$", clean_query_string))
--- app/elastic/parsers/siret.py
+++ app/elastic/parsers/siret.py
@@ -1,4 +1,4 @@
-SIRET_LENGTH = 14  # Constant for the length of a SIRET number
+import re
 
 
 def is_siret(query_string: str) -> bool:
@@ -7,11 +7,6 @@ def is_siret(query_string: str) -> bool:
     """
     if query_string is None or not isinstance(query_string, str):
         return False
-
     clean_query_string = query_string.replace(" ", "")
-
-    # Validate length and digit-only condition
-    if len(clean_query_string) == SIRET_LENGTH and clean_query_string.isdigit():
-        return True
-
-    return False
+    # Using regular expression to check for exactly 9 digits
+    return bool(re.fullmatch(r"^\d{14}$", clean_query_string))

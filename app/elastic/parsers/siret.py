cce8f819dc8d6fff4cc32b56e21a9537b605c192
HAEKADI <hajar.aitelkadi@proton.me>
2024-11-14 10:43:22 +0100

    fix: typo in commnet

--- app/elastic/parsers/siret.py
+++ app/elastic/parsers/siret.py
@@ -8,5 +8,5 @@ def is_siret(query_string: str) -> bool:
     if query_string is None or not isinstance(query_string, str):
         return False
     clean_query_string = query_string.replace(" ", "")
-    # Using regular expression to check for exactly 9 digits
+    # Using regular expression to check for exactly 14 digits
     return bool(re.fullmatch(r"^\d{14}$", clean_query_string))

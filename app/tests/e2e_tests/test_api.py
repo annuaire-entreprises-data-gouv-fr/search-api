a8a6968f85ea7013c45c0964cf3d8c2f91c52716
Hajar AEK <48837850+HAEKADI@users.noreply.github.com>
2025-12-30 16:48:18 +0100

    feat(rne): add `date_fin_existence` to immat object in payload (#609)
    depends on
    https://github.com/annuaire-entreprises-data-gouv-fr/search-infra/pull/635
    
    ⚠️ For some reason in RNE- flux files I can't find the last updates to
    `La Poste` as if they skip it (updates in October and December).

--- app/tests/e2e_tests/test_api.py
+++ app/tests/e2e_tests/test_api.py
@@ -792,29 +792,34 @@ def test_immatriculation(api_response_tester):
     """
     Test immatriculation object.
     """
-    # Test for "la poste" : deactivated awaiting inpi response
-    """
-    path_la_poste = "search?q=la%20poste&include_admin=immatriculation"
-    api_response_tester.assert_api_response_code_200(path_la_poste)
+    # Test "EDF"
+    path_edf = "/search?q=electricite%20de%20france&include_admin=immatriculation"
+    api_response_tester.assert_api_response_code_200(path_edf)
 
-    immatriculation_data_la_poste = {
-        "duree_personne_morale": 99,
-        "date_immatriculation": "1992-03-19",
-        "date_debut_activite": "1991-01-01",
+    immatriculation_data_edf = {
+        "duree_personne_morale": 148,
+        "date_immatriculation": "1955-06-17",
+        "date_debut_activite": None,
         "capital_variable": False,
         "devise_capital": "EUR",
         "indicateur_associe_unique": False,
-        "capital_social": 5857785892,
-        "nature_entreprise": ["Commerciale"],
+        "capital_social": 2084365041,
+        "nature_entreprise": [
+            "Agent commercial",
+            "Libérale réglementée",
+            "Libérale non réglementée",
+            "Gestion de biens",
+            "Commerciale",
+        ],
         "date_cloture_exercice": "3112",
         "date_radiation": None,
+        "date_fin_existence": "2103-11-19",
     }
 
-    for field, expected_value in immatriculation_data_la_poste.items():
+    for field, expected_value in immatriculation_data_edf.items():
         api_response_tester.test_field_value(
-            path_la_poste, 0, f"immatriculation.{field}", expected_value
+            path_edf, 0, f"immatriculation.{field}", expected_value
         )
-    """
 
     # Test for "ganymede"
     path_gan = "search?q=880878145&include_admin=immatriculation"
@@ -825,6 +830,7 @@ def test_immatriculation(api_response_tester):
         "date_immatriculation": "2020-01-23",
         "date_radiation": "2022-11-14",
         "duree_personne_morale": 99,
+        # "date_fin_existence": "2119-01-22", # data not included in RNE API
         "nature_entreprise": ["Commerciale"],
         "date_cloture_exercice": "3112",
         "capital_social": 1000.0,

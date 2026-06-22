9e3c824fc9960140e223328834514008f5524cdf
HAEKADI <hajar.aitelkadi@proton.me>
2026-06-22 19:43:55 +0200

    chore: remove backward compatibilty field tva

--- app/tests/e2e_tests/test_api.py
+++ app/tests/e2e_tests/test_api.py
@@ -1117,14 +1117,6 @@ def test_bodacc(api_response_tester):
 
 
 def test_tva(api_response_tester):
-    path = "search?q=979925039&include_admin=tva"
-    api_response_tester.assert_api_response_code_200(path)
-    response = api_response_tester.get_api_response(path)
-    results = response.json()["results"]
-    for result in results:
-        assert len(result["tva"]) > 1, (
-            "tva should not be empty when filtering by include_admin=tva"
-        )
     path = "search?q=979925039&minimal=true&include=tva"
     api_response_tester.assert_api_response_code_200(path)
     response = api_response_tester.get_api_response(path)
